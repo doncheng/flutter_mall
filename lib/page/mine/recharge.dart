19f18467ba13f1c43bafcd4a1a05557cacfe6c97
HHH402326539 <68835470+HHH402326539@users.noreply.github.com>
2020-09-02 08:44:20 +0800

    支付页面

---
+++ lib/page/mine/recharge.dart
@@ -0,0 +1,385 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class RechargePage extends StatelessWidget {
+  const RechargePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('充值'),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            Container(
+              height: 650,
+              child: ListView(
+                children: [
+                  //
+                  Amnout(),
+                  //
+                  Pay(),
+                  //
+                ],
+              ),
+            ),
+            Container(
+              height: 40,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text('合计'),
+                  Container(
+                    height: 30,
+                    width: 30,
+                    child: Text(""),
+                  ),
+                  Container(
+                    height: 32,
+                    width: 80,
+                    child: RaisedButton(
+                        child: Text(
+                          "支付",
+                          style: TextStyle(color: Colors.white, fontSize: 14),
+                        ),
+                        color: Colors.red,
+                        onPressed: () {}),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
+  }
+}
+
+class Amnout extends StatefulWidget {
+  Amnout({Key key}) : super(key: key);
+
+  @override
+  _AmnoutState createState() => _AmnoutState();
+}
+
+class _AmnoutState extends State<Amnout> {
+  int _chick = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 230,
+      width: double.infinity,
+      margin: EdgeInsets.only(left: 20, top: 25, right: 20),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text(
+                '魅道充值',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+            ),
+            controller: TextEditingController(text: "请输入充值金额"),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Row(children: [
+            Container(
+              height: 50,
+              width: 120,
+              margin: EdgeInsets.only(right: 32),
+              child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Text('50元'),
+                  color: this._chick == 1 ? Colors.red : Colors.white,
+                  onPressed: () {
+                    setState(() {
+                      this._chick = 1;
+                    });
+                  }),
+            ),
+            Container(
+              height: 50,
+              width: 120,
+              margin: EdgeInsets.only(right: 32),
+              child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Text('200元'),
+                  color: this._chick == 2 ? Colors.red : Colors.white,
+                  onPressed: () {
+                    setState(() {
+                      this._chick = 2;
+                    });
+                  }),
+            ),
+            Container(
+              height: 50,
+              width: 120,
+              child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Text('500元'),
+                  color: this._chick == 3 ? Colors.red : Colors.white,
+                  onPressed: () {
+                    setState(() {
+                      this._chick = 3;
+                    });
+                  }),
+            )
+          ]),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Container(
+                height: 50,
+                width: 120,
+                margin: EdgeInsets.only(right: 32),
+                child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    child: Text('1000元'),
+                    color: this._chick == 4 ? Colors.red : Colors.white,
+                    onPressed: () {
+                      setState(() {
+                        this._chick = 4;
+                      });
+                    }),
+              ),
+              Container(
+                height: 50,
+                width: 120,
+                margin: EdgeInsets.only(right: 32),
+                child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    child: Text('1500元'),
+                    color: this._chick == 5 ? Colors.red : Colors.white,
+                    onPressed: () {
+                      setState(() {
+                        this._chick = 5;
+                      });
+                    }),
+              ),
+              Container(
+                height: 50,
+                width: 120,
+                child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    child: Text('1998元'),
+                    color: this._chick == 6 ? Colors.red : Colors.white,
+                    onPressed: () {
+                      setState(() {
+                        this._chick = 6;
+                      });
+                    }),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Pay extends StatefulWidget {
+  Pay({Key key}) : super(key: key);
+
+  @override
+  _PayState createState() => _PayState();
+}
+
+class _PayState extends State<Pay> {
+  var flag = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        margin: EdgeInsets.only(left: 20, top: 25, right: 20),
+        child: Column(children: [
+          Row(
+            children: [
+              Text(
+                '充值数量',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+            ),
+            controller: TextEditingController(text: "请输入充值数量"),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              Text(
+                '充值方式',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Container(
+                width: 380,
+                child: Column(
+                  children: [
+                    Container(
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 40,
+                            width: 40,
+                            child: Image.network(
+                                "https://kf.qq.com/img/wechat.png"),
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Text(
+                            "微信支付",
+                            style: TextStyle(fontSize: 15),
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Container(
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 40,
+                            width: 40,
+                            child: Image.network(
+                                "https://kf.qq.com/img/wechat.png"),
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Text(
+                            "支付宝支付",
+                            style: TextStyle(fontSize: 15),
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Container(
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 40,
+                            width: 40,
+                            child: Image.network(
+                                "https://kf.qq.com/img/wechat.png"),
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Text(
+                            "余额支付",
+                            style: TextStyle(fontSize: 15),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 18,
+                    ),
+                    Radio(
+                      value: 1,
+                      groupValue: this.flag,
+                      onChanged: (v) {
+                        setState(() {
+                          this.flag = v;
+                        });
+                      },
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Radio(
+                      value: 2,
+                      groupValue: this.flag,
+                      onChanged: (v) {
+                        setState(() {
+                          this.flag = v;
+                        });
+                      },
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Radio(
+                      value: 3,
+                      groupValue: this.flag,
+                      onChanged: (v) {
+                        setState(() {
+                          this.flag = v;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          Container(
+            color: Color(0xffF5F5F5),
+            child: TextField(
+              maxLines: 8,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+            ),
+          )
+        ]));
+  }
+}
