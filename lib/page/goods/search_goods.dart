cb14fb9b0b5ee6cf13c0e438cefbaae7f80cd540
HHH402326539 <68835470+HHH402326539@users.noreply.github.com>
2020-09-10 15:46:02 +0800

    搜索页面

--- lib/page/goods/search_goods.dart
+++ lib/page/goods/search_goods.dart
@@ -19,6 +19,8 @@ class _SearchGoodsViewState extends State<SearchGoodsView> {
   List<String> _keywords;
   List<GoodsEntity> _goods;
   GoodsService _goodsService = GoodsService();
+  String selectedValue = '宝贝';
+  String text = '苹果11';
   var _page = 1;
   var _limit = 10;
   var _sortName = Strings.SORT_NAME;
@@ -27,8 +29,7 @@ class _SearchGoodsViewState extends State<SearchGoodsView> {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
-      statusBarColor:
-          Colors.deepOrangeAccent, //or set color with: Color(0xFF0000FF)
+      statusBarColor: Color(0xffffffff), //or set color with: Color(0xFF0000FF)
     ));
     return Scaffold(
       body: SafeArea(
@@ -36,162 +37,233 @@ class _SearchGoodsViewState extends State<SearchGoodsView> {
           children: <Widget>[
             Container(
               height: ScreenUtil.instance.setHeight(100.0),
-              color: Colors.deepOrangeAccent,
-              child: Container(
-                  margin: EdgeInsets.only(
-                      left: ScreenUtil.instance.setWidth(60.0),
-                      top: ScreenUtil.instance.setHeight(10.0),
-                      bottom: ScreenUtil.instance.setHeight(10.0),
-                      right: ScreenUtil.instance.setWidth(60.0)),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(
-                        color: Colors.white,
-                        width: ScreenUtil.instance.setWidth(1.0)),
-                    borderRadius: BorderRadius.circular(
-                        ScreenUtil.instance.setWidth(40.0)),
-                  ),
-                  child: TextField(
-                    onEditingComplete: _sort,
-                    controller: _editingController,
-                    textInputAction: TextInputAction.search,
-                    style: TextStyle(
-                        color: Colors.black54,
-                        fontSize: ScreenUtil.instance.setSp(26.0)),
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(
-                        Icons.search,
-                        size: ScreenUtil.instance.setWidth(50.0),
-                        color: Colors.deepOrangeAccent,
+              color: Color(0xffffffff),
+              child: Row(
+                children: [
+                  Container(
+                      margin: EdgeInsets.only(
+                          left: ScreenUtil.instance.setWidth(60.0),
+                          top: ScreenUtil.instance.setHeight(10.0),
+                          bottom: ScreenUtil.instance.setHeight(10.0),
+                          right: ScreenUtil.instance.setWidth(30.0)),
+                      decoration: BoxDecoration(
+                        color: Color(0xfff0f0f0),
+                        border: Border.all(
+                            color: Colors.blue,
+                            width: ScreenUtil.instance.setWidth(1.0)),
+                        borderRadius: BorderRadius.circular(
+                            ScreenUtil.instance.setWidth(40.0)),
                       ),
-                      hintText: Strings.GOODS_SEARCH_HINT,
-                      hintStyle: TextStyle(
-                          color: Colors.grey,
-                          fontSize: ScreenUtil.instance.setSp(26.0)),
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.transparent),
-                      ),
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.transparent),
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 40,
+                            width: 55,
+                            margin: EdgeInsets.only(left: 10),
+                            child: Center(
+                              child: Row(
+                                children: [
+                                  PopupMenuButton(
+                                    child: Text(selectedValue,
+                                        style: TextStyle(
+                                          fontSize: 14,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.normal,
+                                        )),
+                                    // tooltip: "长按提示",
+                                    initialValue: "goods",
+                                    padding: EdgeInsets.all(0.0),
+                                    itemBuilder: (BuildContext context) {
+                                      return <PopupMenuItem<String>>[
+                                        PopupMenuItem<String>(
+                                          child: Text("宝贝"),
+                                          value: "goods",
+                                        ),
+                                        PopupMenuItem<String>(
+                                          child: Text("商家"),
+                                          value: "users",
+                                        ),
+                                      ];
+                                    },
+                                    onSelected: (String action) {
+                                      switch (action) {
+                                        case "goods":
+                                          setState(() {
+                                            selectedValue = '宝贝';
+                                          });
+                                          break;
+                                        case "users":
+                                          setState(() {
+                                            selectedValue = '商家';
+                                          });
+                                          break;
+                                      }
+                                    },
+                                    onCanceled: () {
+                                      print("onCanceled");
+                                    },
+                                  ),
+                                  Icon(Icons.arrow_drop_down)
+                                ],
+                              ),
+                            ),
+                          ),
+                          Container(
+                            height: 40,
+                            width: 200,
+                            child: TextField(
+                              onEditingComplete: _sort,
+                              controller: _editingController,
+                              textInputAction: TextInputAction.search,
+                              style: TextStyle(
+                                  color: Colors.black54,
+                                  fontSize: ScreenUtil.instance.setSp(26.0)),
+                              decoration: InputDecoration(
+                                prefixIcon: Icon(
+                                  Icons.search,
+                                  size: ScreenUtil.instance.setWidth(50.0),
+                                  color: Colors.grey,
+                                ),
+                                hintText: text,
+                                hintStyle: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: ScreenUtil.instance.setSp(26.0)),
+                                focusedBorder: UnderlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.transparent),
+                                ),
+                                enabledBorder: UnderlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.transparent),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      )),
+                  Container(
+                    height: 40,
+                    width: 50,
+                    child: Center(
+                      child: Text(
+                        '取消',
+                        style:
+                            TextStyle(fontSize: 16, color: Color(0xffff3e45)),
                       ),
                     ),
-                  )),
+                  )
+                ],
+              ),
             ),
             _sortView(),
-            Container(
-                height: double.infinity,
-                margin:
-                    EdgeInsets.only(top: ScreenUtil.instance.setHeight(210.0)),
-                child: _goods == null || _goods.length == 0
-                    ? EmptyView()
-                    : GridView.builder(
-                        itemCount: _goods.length,
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                            childAspectRatio: 1.0,
-                            crossAxisSpacing:
-                                ScreenUtil.instance.setWidth(10.0),
-                            mainAxisSpacing:
-                                ScreenUtil.instance.setHeight(10.0),
-                            crossAxisCount: 2),
-                        itemBuilder: (BuildContext context, int index) {
-                          return getGoodsItemView(_goods[index]);
-                        }))
+            // Container(
+            //     height: double.infinity,
+            //     margin:
+            //         EdgeInsets.only(top: ScreenUtil.instance.setHeight(210.0)),
+            //     child: _goods == null || _goods.length == 0
+            //         ? EmptyView()
+            //         : GridView.builder(
+            //             itemCount: _goods.length,
+            //             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            //                 childAspectRatio: 1.0,
+            //                 crossAxisSpacing:
+            //                     ScreenUtil.instance.setWidth(10.0),
+            //                 mainAxisSpacing:
+            //                     ScreenUtil.instance.setHeight(10.0),
+            //                 crossAxisCount: 2),
+            //             itemBuilder: (BuildContext context, int index) {
+            //               return getGoodsItemView(_goods[index]);
+            //             }))
           ],
         ),
       ),
     );
   }
 
+  List list = [
+    {"name": '洗衣机'},
+    {"name": '苹果7'},
+    {"name": '华为'},
+    {"name": 'ps4'},
+    {"name": '电瓶车'},
+    {"name": '大疆'},
+    {"name": '荣耀play'},
+    {"name": '三星s10'},
+  ];
+
+  //自定义方法
+  List<Widget> _getData() {
+//循环
+    var tempList = list.map((value) {
+      return InkWell(
+        onTap: () {
+          setState(() {
+            text = value["name"];
+            print(value["name"]);
+          });
+        },
+        child: Container(
+          height: 24,
+          margin: EdgeInsets.only(left: 10),
+          padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(6), color: Color(0xfff0f0f0)),
+          child: Text(
+            value["name"],
+            style: TextStyle(fontSize: 12),
+          ),
+        ),
+      );
+    });
+    return tempList.toList();
+  }
+
   Widget _sortView() {
     return Container(
-      margin: EdgeInsets.only(top: ScreenUtil.instance.setHeight(100.0)),
-      height: ScreenUtil.instance.setHeight(100.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Expanded(
-              flex: 1,
-              child: InkWell(
-                onTap: () => _comprehensive(),
-                child: Container(
-                  decoration: BoxDecoration(
-                      border: Border(
-                          right: BorderSide(
-                              color: Colors.deepOrangeAccent,
-                              width: ScreenUtil.instance.setWidth(1.0)))),
-                  alignment: Alignment.center,
-                  child: Text(
-                    Strings.COMPREHENSIVE,
-                    style: TextStyle(
-                        color: Colors.black54,
-                        fontSize: ScreenUtil.instance.setSp(26.0)),
-                  ),
+        margin: EdgeInsets.only(top: ScreenUtil.instance.setHeight(100.0)),
+        height: ScreenUtil.instance.setHeight(200.0),
+        child: Column(
+          children: [
+            Container(
+              height: 22,
+              margin: EdgeInsets.only(left: 15, bottom: 10, top: 10),
+              width: double.infinity,
+              child: Text(
+                '推荐搜索',
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
                 ),
-              )),
-          Expanded(
-              flex: 1,
-              child: Container(
-                alignment: Alignment.center,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      Strings.SEARCH_GOODS_PRICE,
-                      style: TextStyle(
-                          fontSize: ScreenUtil.instance.setSp(26.0),
-                          color: Colors.black54),
-                    ),
-                    Padding(
-                        padding: EdgeInsets.only(
-                            left: ScreenUtil.instance.setWidth(10.0))),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        InkWell(
-                          onTap: () => _priveSort(1),
-                          child: Container(
-                            height: ScreenUtil.instance.setHeight(50.0),
-                            alignment: Alignment.center,
-                            child: Icon(
-                              Icons.keyboard_arrow_up,
-                              size: ScreenUtil.instance.setHeight(30.0),
-                            ),
-                          ),
-                        ),
-                        InkWell(
-                          onTap: () => _priveSort(2),
-                          child: Container(
-                            height: ScreenUtil.instance.setHeight(50.0),
-                            alignment: Alignment.center,
-                            child: Icon(
-                              Icons.keyboard_arrow_down,
-                              size: ScreenUtil.instance.setHeight(30.0),
-                            ),
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              ))
-        ],
-      ),
-    );
+                textAlign: TextAlign.left,
+              ),
+            ),
+            Container(
+              height: 60,
+              width: double.infinity,
+              child: Wrap(
+                //元素横轴之间的距离
+                spacing: 10,
+//纵轴的距离
+                runSpacing: 10,
+                children: _getData(),
+              ),
+            )
+          ],
+        ));
   }
 
-  //点击综合
-  _comprehensive() {
-    _sortName = Strings.SORT_NAME;
-    _orderType = Strings.DESC;
-    _sort();
-  }
+  // //点击综合
+  // _comprehensive() {
+  //   _sortName = Strings.SORT_NAME;
+  //   _orderType = Strings.DESC;
+  //   _sort();
+  // }
 
-  _priveSort(int type) {
-    _sortName = Strings.SORT_RETAIL_PRICE;
-    _orderType = type == 1 ? Strings.ASC : Strings.DESC;
-    _sort();
-  }
+  // _priveSort(int type) {
+  //   _sortName = Strings.SORT_RETAIL_PRICE;
+  //   _orderType = type == 1 ? Strings.ASC : Strings.DESC;
+  //   _sort();
+  // }
 
   _sort() {
     FocusScope.of(context).requestFocus(FocusNode()); //隐藏键盘
@@ -252,17 +324,21 @@ class _SearchGoodsViewState extends State<SearchGoodsView> {
                     goodsEntity.name,
                     maxLines: 1,
                     overflow: TextOverflow.ellipsis,
-                    style: TextStyle(fontSize: ScreenUtil.getInstance().setSp(26.0), color: Colors.black54),
+                    style: TextStyle(
+                        fontSize: ScreenUtil.getInstance().setSp(26.0),
+                        color: Colors.black54),
                   ),
                   Padding(
-                    padding: EdgeInsets.only(top:ScreenUtil.getInstance().setHeight(10.0)),
+                    padding: EdgeInsets.only(
+                        top: ScreenUtil.getInstance().setHeight(10.0)),
                   ),
                   Text(
                     "¥${goodsEntity.retailPrice}",
                     maxLines: 1,
                     overflow: TextOverflow.ellipsis,
                     style: TextStyle(
-                        fontSize: ScreenUtil.getInstance().setSp(26.0), color: Colors.deepOrangeAccent),
+                        fontSize: ScreenUtil.getInstance().setSp(26.0),
+                        color: Colors.deepOrangeAccent),
                   ),
                 ],
               ),
