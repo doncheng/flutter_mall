5338dd2ae5e65934950dc23917211b69666803a9
HHH402326539 <68835470+HHH402326539@users.noreply.github.com>
2020-09-23 12:08:01 +0800

    验证码

--- lib/page/home/user_phone.dart
+++ lib/page/home/user_phone.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'dart:async';
 
 class UserPhonePage extends StatelessWidget {
   const UserPhonePage({Key key}) : super(key: key);
@@ -34,6 +36,35 @@ class PhoneBody extends StatefulWidget {
 }
 
 class _PhoneBodyState extends State<PhoneBody> {
+  Timer _timer;
+  int _countdownTime = 0;
+
+  void startCountdownTimer() {
+    const oneSec = const Duration(seconds: 1);
+
+    var callback = (timer) => {
+          setState(() {
+            if (_countdownTime < 1) {
+              _timer.cancel();
+            } else {
+              _countdownTime = _countdownTime - 1;
+            }
+          })
+        };
+    _timer = Timer.periodic(oneSec, callback);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    if (_timer != null) {
+      _timer.cancel();
+    }
+  }
+
+  String phone_number = "";
+  String code = '';
+  //
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -49,6 +80,13 @@ class _PhoneBodyState extends State<PhoneBody> {
           height: 40,
           padding: EdgeInsets.only(left: 10, top: 20, bottom: 0),
           child: TextField(
+            inputFormatters: [
+              // ignore: deprecated_member_use
+              WhitelistingTextInputFormatter(RegExp("[0-9.]")), //只允许输入小数
+            ],
+            onChanged: (number) {
+              phone_number = number;
+            },
             style: TextStyle(fontSize: 14, color: Color(0xffbfbfbf)),
             decoration:
                 InputDecoration(border: InputBorder.none, hintText: '绑定手机号'),
@@ -70,6 +108,9 @@ class _PhoneBodyState extends State<PhoneBody> {
                 height: 20,
                 width: 150,
                 child: TextField(
+                  onChanged: (value) {
+                    code = value;
+                  },
                   style: TextStyle(fontSize: 14, color: Color(0xffbfbfbf)),
                   decoration: InputDecoration(
                       border: InputBorder.none, hintText: '请输入验证码'),
@@ -80,15 +121,36 @@ class _PhoneBodyState extends State<PhoneBody> {
                   margin:
                       EdgeInsets.only(left: 10, top: 10, right: 20, bottom: 10),
                   height: 20,
-                  width: 60,
+                  width: 100,
                   child: Center(
                     child: Text(
-                      '发送验证码',
-                      style: TextStyle(fontSize: 12, color: Colors.red),
+                      _countdownTime > 0 ? '$_countdownTime后重新获取' : '获取验证码',
+                      style: TextStyle(
+                          fontSize: 12,
+                          color: _countdownTime > 0
+                              ? Color.fromARGB(255, 183, 184, 195)
+                              : Colors.blue),
                     ),
                   ),
                 ),
-                onTap: () {},
+                onTap: () {
+                  if (_countdownTime == 0) {
+                    //Http请求发送验证码
+                    //...
+                    Fluttertoast.showToast(
+                        msg: "发送成功，正在获取...",
+                        toastLength: Toast.LENGTH_SHORT,
+                        gravity: ToastGravity.BOTTOM,
+                        timeInSecForIos: 1,
+                        textColor: Colors.white,
+                        backgroundColor: Colors.grey);
+                    setState(() {
+                      _countdownTime = 60;
+                    });
+                    //开始倒计时
+                    startCountdownTimer();
+                  }
+                },
               )
             ],
           )),
@@ -106,15 +168,39 @@ class _PhoneBodyState extends State<PhoneBody> {
               style: TextStyle(fontSize: 20, color: Colors.white),
             ),
             onPressed: () {
-              // Fluttertoast.showToast(
-              //   msg: "网络连接错误",
-              //   toastLength: Toast.LENGTH_SHORT,
-              //   gravity: ToastGravity.BOTTOM,
-              //   timeInSecForIos: 1,
-              //   textColor: Colors.white,
-              //   backgroundColor: Colors.yellow
-
-              // );
+              if (phone_number == "" || phone_number.length < 11) {
+                Fluttertoast.showToast(
+                    msg: "请输入正确的手机号",
+                    toastLength: Toast.LENGTH_SHORT,
+                    gravity: ToastGravity.BOTTOM,
+                    timeInSecForIos: 1,
+                    textColor: Colors.white,
+                    backgroundColor: Colors.grey);
+              } else if (phone_number == "") {
+                Fluttertoast.showToast(
+                    msg: "手机号不能为空",
+                    toastLength: Toast.LENGTH_SHORT,
+                    gravity: ToastGravity.BOTTOM,
+                    timeInSecForIos: 1,
+                    textColor: Colors.white,
+                    backgroundColor: Colors.grey);
+              } else if (code == "") {
+                Fluttertoast.showToast(
+                    msg: "请输入验证码",
+                    toastLength: Toast.LENGTH_SHORT,
+                    gravity: ToastGravity.BOTTOM,
+                    timeInSecForIos: 1,
+                    textColor: Colors.white,
+                    backgroundColor: Colors.grey);
+              } else {
+                Fluttertoast.showToast(
+                    msg: "网络连接错误",
+                    toastLength: Toast.LENGTH_SHORT,
+                    gravity: ToastGravity.BOTTOM,
+                    timeInSecForIos: 1,
+                    textColor: Colors.white,
+                    backgroundColor: Colors.grey);
+              }
             },
           ),
         ),
