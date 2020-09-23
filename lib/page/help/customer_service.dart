87dad04f73ddaa5a2b4398fe8b20f868f89efd00
HHH402326539 <68835470+HHH402326539@users.noreply.github.com>
2020-09-23 14:56:49 +0800

    再改

--- lib/page/help/customer_service.dart
+++ lib/page/help/customer_service.dart
@@ -10,13 +10,6 @@ class CustomerPage extends StatefulWidget {
 
 class _CustomerPageState extends State<CustomerPage> {
   @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    print('11111');
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -51,285 +44,127 @@ class _CustomerBodyState extends State<CustomerBody> {
     final sizeheight = size.height;
     return Column(
       children: [
-        Container(
-          color: Colors.white,
-          height: 122,
-          width: double.infinity,
-          child: Center(
-              child: Container(
-            height: 88,
-            width: 211,
-            child: Column(
-              children: [
-                Row(
+        Flexible(
+            child: ListView(
+          children: [
+            Container(
+              color: Colors.white,
+              height: 122,
+              width: double.infinity,
+              child: Center(
+                  child: Container(
+                height: 88,
+                width: 211,
+                child: Column(
                   children: [
-                    Container(
-                      margin: EdgeInsets.fromLTRB(10, 10, 10, 6),
-                      color: Color(0xffbfbfbf),
-                      height: 40,
-                      width: 40,
-                      child: Image.network(
-                        'https://i0.hdslb.com/bfs/archive/31b8d2617cb8d6b01e98425b7eba39ae9f973c20.png',
-                        fit: BoxFit.cover,
-                      ),
+                    Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.fromLTRB(10, 10, 10, 6),
+                          color: Color(0xffbfbfbf),
+                          height: 40,
+                          width: 40,
+                          child: Image.network(
+                            'https://i0.hdslb.com/bfs/archive/31b8d2617cb8d6b01e98425b7eba39ae9f973c20.png',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 88,
+                        ),
+                        Container(
+                          margin: EdgeInsets.fromLTRB(10, 10, 10, 6),
+                          color: Color(0xffbfbfbf),
+                          height: 40,
+                          width: 40,
+                          child: Image.network(
+                            'https://i0.hdslb.com/bfs/archive/31b8d2617cb8d6b01e98425b7eba39ae9f973c20.png',
+                            fit: BoxFit.cover,
+                          ),
+                        )
+                      ],
                     ),
-                    SizedBox(
-                      width: 88,
-                    ),
-                    Container(
-                      margin: EdgeInsets.fromLTRB(10, 10, 10, 6),
-                      color: Color(0xffbfbfbf),
-                      height: 40,
-                      width: 40,
-                      child: Image.network(
-                        'https://i0.hdslb.com/bfs/archive/31b8d2617cb8d6b01e98425b7eba39ae9f973c20.png',
-                        fit: BoxFit.cover,
-                      ),
+                    Row(
+                      children: [
+                        Container(
+                          height: 20,
+                          width: 56,
+                          child: Text(
+                            "我要提现",
+                            style: TextStyle(fontSize: 14),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 85,
+                        ),
+                        Container(
+                          height: 20,
+                          width: 70,
+                          child: Text(
+                            "我要催发货",
+                            style: TextStyle(fontSize: 14),
+                          ),
+                        ),
+                      ],
                     )
                   ],
                 ),
-                Row(
-                  children: [
-                    Container(
-                      height: 20,
-                      width: 56,
-                      child: Text(
-                        "我要提现",
-                        style: TextStyle(fontSize: 14),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 85,
-                    ),
-                    Container(
-                      height: 20,
-                      width: 70,
-                      child: Text(
-                        "我要催发货",
-                        style: TextStyle(fontSize: 14),
-                      ),
-                    ),
-                  ],
-                )
-              ],
+              )),
             ),
-          )),
-        ),
-        SizedBox(
-          height: 6,
-        ),
-        Container(
-            color: Colors.white,
-            width: double.infinity,
-            constraints: BoxConstraints(maxHeight: double.infinity),
-            child: Column(
-              children: [
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            //导航打开新视图
-                            builder: (context) => ProblemPage()));
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(left: 20, top: 10),
-                        height: 20,
-                        width: 56,
-                        child: Text(
-                          '常见问题',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w600, fontSize: 14),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(right: 20, top: 10),
-                        height: 20,
-                        width: 84,
-                        child: Text(
-                          '查看所有问题',
-                          style:
-                              TextStyle(fontSize: 14, color: Color(0xffbfbfbf)),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                Divider(),
-                InkWell(
-                  onTap: () {},
-                  child: Container(
-                    margin: EdgeInsets.only(top: 10),
-                    padding: EdgeInsets.only(left: 20),
-                    height: 42,
-                    width: double.infinity,
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text('账号被找回，怎么办？',
-                                style: TextStyle(
-                                  fontSize: 14,
-                                )),
-                            Container(
-                              margin: EdgeInsets.only(right: 20),
-                              child: Icon(Icons.arrow_right),
-                            )
-                          ],
-                        ),
-                        Divider()
-                      ],
+            SizedBox(
+              height: 6,
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        //导航打开新视图
+                        builder: (context) => ProblemPage()));
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 20, top: 10),
+                    height: 20,
+                    width: 56,
+                    child: Text(
+                      '常见问题',
+                      style:
+                          TextStyle(fontWeight: FontWeight.w600, fontSize: 14),
                     ),
                   ),
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: Container(
-                    padding: EdgeInsets.only(left: 20),
-                    height: 42,
-                    width: double.infinity,
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text('账号被找回，怎么办？',
-                                style: TextStyle(
-                                  fontSize: 14,
-                                )),
-                            Container(
-                              margin: EdgeInsets.only(right: 20),
-                              child: Icon(Icons.arrow_right),
-                            )
-                          ],
-                        ),
-                        Divider()
-                      ],
+                  Container(
+                    margin: EdgeInsets.only(right: 20, top: 10),
+                    height: 20,
+                    width: 84,
+                    child: Text(
+                      '查看所有问题',
+                      style: TextStyle(fontSize: 14, color: Color(0xffbfbfbf)),
                     ),
+                  )
+                ],
+              ),
+            ),
+            Divider(),
+            Column(
+              children: _getData(),
+            ),
+            Container(
+                constraints: BoxConstraints(maxHeight: double.infinity),
+                color: Color(0xfff5f5f5),
+                child: TextField(
+                  maxLines: 8,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
                   ),
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: Container(
-                    padding: EdgeInsets.only(left: 20),
-                    height: 42,
-                    width: double.infinity,
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text('账号被找回，怎么办？',
-                                style: TextStyle(
-                                  fontSize: 14,
-                                )),
-                            Container(
-                              margin: EdgeInsets.only(right: 20),
-                              child: Icon(Icons.arrow_right),
-                            )
-                          ],
-                        ),
-                        Divider()
-                      ],
-                    ),
-                  ),
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: Container(
-                    padding: EdgeInsets.only(left: 20),
-                    height: 42,
-                    width: double.infinity,
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text('账号被找回，怎么办？',
-                                style: TextStyle(
-                                  fontSize: 14,
-                                )),
-                            Container(
-                              margin: EdgeInsets.only(right: 20),
-                              child: Icon(Icons.arrow_right),
-                            )
-                          ],
-                        ),
-                        Divider()
-                      ],
-                    ),
-                  ),
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: Container(
-                    padding: EdgeInsets.only(left: 20),
-                    height: 42,
-                    width: double.infinity,
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text('账号被找回，怎么办？',
-                                style: TextStyle(
-                                  fontSize: 14,
-                                )),
-                            Container(
-                              margin: EdgeInsets.only(right: 20),
-                              child: Icon(Icons.arrow_right),
-                            )
-                          ],
-                        ),
-                        Divider()
-                      ],
-                    ),
-                  ),
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: Container(
-                    padding: EdgeInsets.only(left: 20),
-                    height: 42,
-                    width: double.infinity,
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text('账号被找回，怎么办？',
-                                style: TextStyle(
-                                  fontSize: 14,
-                                )),
-                            Container(
-                              margin: EdgeInsets.only(right: 20),
-                              child: Icon(Icons.arrow_right),
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Container(
-                  constraints: BoxConstraints(maxHeight: double.infinity),
-                  color: Color(0xfff5f5f5),
-                  child: TextField(
-                    maxLines: 6,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                    ),
-                  ),
-                )
-              ],
-            )),
+                ))
+          ],
+        )),
         Container(
           height: 50,
-          // margin: EdgeInsets.only(bottom: 50),
+          margin: EdgeInsets.only(bottom: 50),
           child: Center(
             child: Container(
               height: 20,
@@ -354,5 +189,61 @@ class _CustomerBodyState extends State<CustomerBody> {
         )
       ],
     );
+  }
+
+  List list = [
+    {
+      "problem": '账号被找回，怎么办？',
+    },
+    {
+      "problem": '账号被找回，怎么办？',
+    },
+    {
+      "problem": '账号被找回，怎么办？',
+    },
+    {
+      "problem": '账号被找回，怎么办？',
+    },
+    {
+      "problem": '账号被找回，怎么办？',
+    },
+    {
+      "problem": '账号被找回，怎么办？',
+    },
+  ];
+
+  int i = 0;
+//自定义方法
+  List<Widget> _getData() {
+//循环
+    var tempList = list.map((value) {
+      return Container(
+        margin: EdgeInsets.only(top: 10),
+        padding: EdgeInsets.only(left: 20),
+        constraints: BoxConstraints(maxHeight: double.infinity),
+        width: double.infinity,
+        child: Column(
+          children: [
+            InkWell(
+                onTap: () {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(value["problem"],
+                        style: TextStyle(
+                          fontSize: 14,
+                        )),
+                    Container(
+                      margin: EdgeInsets.only(right: 20),
+                      child: Icon(Icons.arrow_right),
+                    )
+                  ],
+                )),
+            Divider()
+          ],
+        ),
+      );
+    });
+    return tempList.toList();
   }
 }
